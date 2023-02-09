7eb5d2ea12cf6ce194699dc4f1bf5f47527f3746
samrez Ikram <samrezikram@yahoo.com>
2023-02-09 18:03:39 +0400

    Initial Commit

---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Samrez Ikram on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
